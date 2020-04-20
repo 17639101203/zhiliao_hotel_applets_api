cfd75356cc7361e1dab24f550975f54704edf934
xiehuiyi <1324719929@qq.com>
2020-04-20 16:17:38 +0800

    包引入

--- src/main/java/com/zhiliao/hotel/controller/clean/ZlCleanOrderController.java
+++ src/main/java/com/zhiliao/hotel/controller/clean/ZlCleanOrderController.java
@@ -1,5 +1,6 @@
 package com.zhiliao.hotel.controller.clean;
 
+import com.zhiliao.hotel.common.PassToken;
 import com.zhiliao.hotel.common.ReturnString;
 import com.zhiliao.hotel.common.UserLoginToken;
 import com.zhiliao.hotel.model.ZlCleanOrder;
