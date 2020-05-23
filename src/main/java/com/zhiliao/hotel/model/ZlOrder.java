74f8602138ad5cc7ccd1473826701fc53584a20e
LinSir <1689686109@qq.com>
2020-05-23 09:41:30 +0800

    修改zl_order、zl_orderdetail表，对应的model

--- src/main/java/com/zhiliao/hotel/model/ZlOrder.java
+++ src/main/java/com/zhiliao/hotel/model/ZlOrder.java
@@ -26,6 +26,11 @@ public class ZlOrder implements Serializable {
     private Long userid;
     
     /**
+     * 父订单编号
+     */
+    private String parentorderserialno;
+    
+    /**
      * 订单编号
      */
     private String orderserialno;
@@ -54,11 +59,6 @@ public class ZlOrder implements Serializable {
      * 第一张商品图片地址
      */
     private String goodscoverurl;
-    
-    /**
-     * 1:便利店;2餐饮服务;3情趣用品;4土特产
-     */
-    private Byte ordertype;
     
     /**
      * 备注信息
@@ -106,7 +106,7 @@ public class ZlOrder implements Serializable {
     private Byte paystatus;
     
     /**
-     * 订单状态：-1:已取消;0待确认;1已确认/已下单;2 已发货；3已签收/已完成;4最终完成(不能被操作)
+     * 订单状态:-2报损;-1:已取消;0待确认;1已确认/已下单;2 已发货；3已签收/已完成;4最终完成(不能被操作)
      */
     private Byte orderstatus;
     
@@ -114,6 +114,46 @@ public class ZlOrder implements Serializable {
      * 退款状态:-2退款被驳回;-1用户取消退款;1审核中;2退款中;3已退款
      */
     private Byte refundstatus;
+    
+    /**
+     * 退款发起次数
+     */
+    private Byte refundcount;
+    
+    /**
+     * 优惠券ID
+     */
+    private Integer couponid;
+    
+    /**
+     * 所属模块: 1便利店;2餐饮服务;3情趣用品;4土特产
+     */
+    private Short belongmodule;
+    
+    /**
+     * 删除状态:0正常;1删除;2用户删除(用户端不显示)
+     */
+    private Boolean isdelete;
+    
+    /**
+     * 下单时间
+     */
+    private Integer createdate;
+    
+    /**
+     * 支付/取消时间
+     */
+    private Integer updatedate;
+    
+    /**
+     * 优惠券金额
+     */
+    private BigDecimal couponcash;
+    
+    /**
+     * 配送金额
+     */
+    private BigDecimal sendcash;
     
     /**
      * 操作员
@@ -130,258 +170,295 @@ public class ZlOrder implements Serializable {
      */
     private String operatorremark;
     
-    /**
-     * 删除状态:0正常;1删除;
-     */
-    private Boolean isdelete;
-    
-    /**
-     * 下单时间
-     */
-    private Integer createdate;
-    
-    /**
-     * 支付/取消时间
-     */
-    private Integer updatedate;
-    
-    /**
-     * 退款发起次数
-     */
-    private Byte refundcount;
-    
-    /**
-     * 优惠券ID
-     */
-    private Integer couponid;
-    
     private static final long serialVersionUID=1L;
-
-    public Long getOrderid() {
+    
+    public Long getOrderid(){
         return orderid;
     }
-
-    public void setOrderid(Long orderid) {
-        this.orderid = orderid;
+    
+    public void setOrderid(Long orderid){
+        this.orderid=orderid;
     }
-
-    public Long getUserid() {
+    
+    public Long getUserid(){
         return userid;
     }
-
-    public void setUserid(Long userid) {
-        this.userid = userid;
+    
+    public void setUserid(Long userid){
+        this.userid=userid;
     }
-
-    public String getOrderserialno() {
+    
+    public String getParentorderserialno(){
+        return parentorderserialno;
+    }
+    
+    public void setParentorderserialno(String parentorderserialno){
+        this.parentorderserialno=parentorderserialno;
+    }
+    
+    public String getOrderserialno(){
         return orderserialno;
     }
-
-    public void setOrderserialno(String orderserialno) {
-        this.orderserialno = orderserialno;
+    
+    public void setOrderserialno(String orderserialno){
+        this.orderserialno=orderserialno;
     }
-
-    public Integer getHotelid() {
+    
+    public Integer getHotelid(){
         return hotelid;
     }
-
-    public void setHotelid(Integer hotelid) {
-        this.hotelid = hotelid;
+    
+    public void setHotelid(Integer hotelid){
+        this.hotelid=hotelid;
     }
-
-    public String getHotelname() {
+    
+    public String getHotelname(){
         return hotelname;
     }
-
-    public void setHotelname(String hotelname) {
-        this.hotelname = hotelname;
+    
+    public void setHotelname(String hotelname){
+        this.hotelname=hotelname;
     }
-
-    public Integer getRoomid() {
+    
+    public Integer getRoomid(){
         return roomid;
     }
-
-    public void setRoomid(Integer roomid) {
-        this.roomid = roomid;
+    
+    public void setRoomid(Integer roomid){
+        this.roomid=roomid;
     }
-
-    public String getRoomnumber() {
+    
+    public String getRoomnumber(){
         return roomnumber;
     }
-
-    public void setRoomnumber(String roomnumber) {
-        this.roomnumber = roomnumber;
+    
+    public void setRoomnumber(String roomnumber){
+        this.roomnumber=roomnumber;
     }
-
-    public String getGoodscoverurl() {
+    
+    public String getGoodscoverurl(){
         return goodscoverurl;
     }
-
-    public void setGoodscoverurl(String goodscoverurl) {
-        this.goodscoverurl = goodscoverurl;
+    
+    public void setGoodscoverurl(String goodscoverurl){
+        this.goodscoverurl=goodscoverurl;
     }
-
-    public Byte getOrdertype() {
-        return ordertype;
-    }
-
-    public void setOrdertype(Byte ordertype) {
-        this.ordertype = ordertype;
-    }
-
-    public String getRemark() {
+    
+    public String getRemark(){
         return remark;
     }
-
-    public void setRemark(String remark) {
-        this.remark = remark;
+    
+    public void setRemark(String remark){
+        this.remark=remark;
     }
-
-    public BigDecimal getTotalprice() {
+    
+    public BigDecimal getTotalprice(){
         return totalprice;
     }
-
-    public void setTotalprice(BigDecimal totalprice) {
-        this.totalprice = totalprice;
+    
+    public void setTotalprice(BigDecimal totalprice){
+        this.totalprice=totalprice;
     }
-
-    public BigDecimal getActuallypay() {
+    
+    public BigDecimal getActuallypay(){
         return actuallypay;
     }
-
-    public void setActuallypay(BigDecimal actuallypay) {
-        this.actuallypay = actuallypay;
+    
+    public void setActuallypay(BigDecimal actuallypay){
+        this.actuallypay=actuallypay;
     }
-
-    public Byte getPaytype() {
+    
+    public Byte getPaytype(){
         return paytype;
     }
-
-    public void setPaytype(Byte paytype) {
-        this.paytype = paytype;
+    
+    public void setPaytype(Byte paytype){
+        this.paytype=paytype;
     }
-
-    public String getDeliveryaddress() {
+    
+    public String getDeliveryaddress(){
         return deliveryaddress;
     }
-
-    public void setDeliveryaddress(String deliveryaddress) {
-        this.deliveryaddress = deliveryaddress;
+    
+    public void setDeliveryaddress(String deliveryaddress){
+        this.deliveryaddress=deliveryaddress;
     }
-
-    public Integer getComeformid() {
+    
+    public Integer getComeformid(){
         return comeformid;
     }
-
-    public void setComeformid(Integer comeformid) {
-        this.comeformid = comeformid;
+    
+    public void setComeformid(Integer comeformid){
+        this.comeformid=comeformid;
     }
-
-    public Integer getExpressid() {
+    
+    public Integer getExpressid(){
         return expressid;
     }
-
-    public void setExpressid(Integer expressid) {
-        this.expressid = expressid;
+    
+    public void setExpressid(Integer expressid){
+        this.expressid=expressid;
     }
-
-    public String getTracknumber() {
+    
+    public String getTracknumber(){
         return tracknumber;
     }
-
-    public void setTracknumber(String tracknumber) {
-        this.tracknumber = tracknumber;
+    
+    public void setTracknumber(String tracknumber){
+        this.tracknumber=tracknumber;
     }
-
-    public Byte getPaystatus() {
+    
+    public Byte getPaystatus(){
         return paystatus;
     }
-
-    public void setPaystatus(Byte paystatus) {
-        this.paystatus = paystatus;
+    
+    public void setPaystatus(Byte paystatus){
+        this.paystatus=paystatus;
     }
-
-    public Byte getOrderstatus() {
+    
+    public Byte getOrderstatus(){
         return orderstatus;
     }
-
-    public void setOrderstatus(Byte orderstatus) {
-        this.orderstatus = orderstatus;
+    
+    public void setOrderstatus(Byte orderstatus){
+        this.orderstatus=orderstatus;
     }
-
-    public Byte getRefundstatus() {
+    
+    public Byte getRefundstatus(){
         return refundstatus;
     }
-
-    public void setRefundstatus(Byte refundstatus) {
-        this.refundstatus = refundstatus;
+    
+    public void setRefundstatus(Byte refundstatus){
+        this.refundstatus=refundstatus;
     }
-
-    public String getOperatorname() {
-        return operatorname;
-    }
-
-    public void setOperatorname(String operatorname) {
-        this.operatorname = operatorname;
-    }
-
-    public String getOperatorip() {
-        return operatorip;
-    }
-
-    public void setOperatorip(String operatorip) {
-        this.operatorip = operatorip;
-    }
-
-    public String getOperatorremark() {
-        return operatorremark;
-    }
-
-    public void setOperatorremark(String operatorremark) {
-        this.operatorremark = operatorremark;
-    }
-
-    public Boolean getIsdelete() {
-        return isdelete;
-    }
-
-    public void setIsdelete(Boolean isdelete) {
-        this.isdelete = isdelete;
-    }
-
-    public Integer getCreatedate() {
-        return createdate;
-    }
-
-    public void setCreatedate(Integer createdate) {
-        this.createdate = createdate;
-    }
-
-    public Integer getUpdatedate() {
-        return updatedate;
-    }
-
-    public void setUpdatedate(Integer updatedate) {
-        this.updatedate = updatedate;
-    }
-
-    public Byte getRefundcount() {
+    
+    public Byte getRefundcount(){
         return refundcount;
     }
-
-    public void setRefundcount(Byte refundcount) {
-        this.refundcount = refundcount;
+    
+    public void setRefundcount(Byte refundcount){
+        this.refundcount=refundcount;
     }
-
-    public Integer getCouponid() {
+    
+    public Integer getCouponid(){
         return couponid;
     }
-
-    public void setCouponid(Integer couponid) {
-        this.couponid = couponid;
+    
+    public void setCouponid(Integer couponid){
+        this.couponid=couponid;
     }
-
-    public static long getSerialVersionUID() {
+    
+    public Short getBelongmodule(){
+        return belongmodule;
+    }
+    
+    public void setBelongmodule(Short belongmodule){
+        this.belongmodule=belongmodule;
+    }
+    
+    public Boolean getIsdelete(){
+        return isdelete;
+    }
+    
+    public void setIsdelete(Boolean isdelete){
+        this.isdelete=isdelete;
+    }
+    
+    public Integer getCreatedate(){
+        return createdate;
+    }
+    
+    public void setCreatedate(Integer createdate){
+        this.createdate=createdate;
+    }
+    
+    public Integer getUpdatedate(){
+        return updatedate;
+    }
+    
+    public void setUpdatedate(Integer updatedate){
+        this.updatedate=updatedate;
+    }
+    
+    public BigDecimal getCouponcash(){
+        return couponcash;
+    }
+    
+    public void setCouponcash(BigDecimal couponcash){
+        this.couponcash=couponcash;
+    }
+    
+    public BigDecimal getSendcash(){
+        return sendcash;
+    }
+    
+    public void setSendcash(BigDecimal sendcash){
+        this.sendcash=sendcash;
+    }
+    
+    public String getOperatorname(){
+        return operatorname;
+    }
+    
+    public void setOperatorname(String operatorname){
+        this.operatorname=operatorname;
+    }
+    
+    public String getOperatorip(){
+        return operatorip;
+    }
+    
+    public void setOperatorip(String operatorip){
+        this.operatorip=operatorip;
+    }
+    
+    public String getOperatorremark(){
+        return operatorremark;
+    }
+    
+    public void setOperatorremark(String operatorremark){
+        this.operatorremark=operatorremark;
+    }
+    
+    public static long getSerialVersionUID(){
         return serialVersionUID;
     }
+    
+    @Override
+    public String toString(){
+        return "ZlOrder{"+
+                "orderid="+orderid+
+                ", userid="+userid+
+                ", parentorderserialno='"+parentorderserialno+'\''+
+                ", orderserialno='"+orderserialno+'\''+
+                ", hotelid="+hotelid+
+                ", hotelname='"+hotelname+'\''+
+                ", roomid="+roomid+
+                ", roomnumber='"+roomnumber+'\''+
+                ", goodscoverurl='"+goodscoverurl+'\''+
+                ", remark='"+remark+'\''+
+                ", totalprice="+totalprice+
+                ", actuallypay="+actuallypay+
+                ", paytype="+paytype+
+                ", deliveryaddress='"+deliveryaddress+'\''+
+                ", comeformid="+comeformid+
+                ", expressid="+expressid+
+                ", tracknumber='"+tracknumber+'\''+
+                ", paystatus="+paystatus+
+                ", orderstatus="+orderstatus+
+                ", refundstatus="+refundstatus+
+                ", refundcount="+refundcount+
+                ", couponid="+couponid+
+                ", belongmodule="+belongmodule+
+                ", isdelete="+isdelete+
+                ", createdate="+createdate+
+                ", updatedate="+updatedate+
+                ", couponcash="+couponcash+
+                ", sendcash="+sendcash+
+                ", operatorname='"+operatorname+'\''+
+                ", operatorip='"+operatorip+'\''+
+                ", operatorremark='"+operatorremark+'\''+
+                '}';
+    }
+    
 }
--- src/main/java/com/zhiliao/hotel/model/ZlOrderDetail.java
+++ src/main/java/com/zhiliao/hotel/model/ZlOrderDetail.java
@@ -12,13 +12,14 @@ import java.math.BigDecimal;
  * @author null
  * @date 2020-04-15
  */
-@Table(name = "zl_orderdetail")
-public class ZlOrderDetail implements Serializable {
+@Table(name="zl_orderdetail")
+public class ZlOrderDetail implements Serializable{
+    
     /**
      * 自增ID
      */
     @Id
-    @GeneratedValue(generator = "JDBC")
+    @GeneratedValue(generator="JDBC")
     private Long orderdetailid;
     
     /**
@@ -57,7 +58,12 @@ public class ZlOrderDetail implements Serializable {
     private Integer goodscount;
     
     /**
-     * 删除状态:0正常;1删除;
+     * 所属模块: 1便利店;2餐饮服务;3情趣用品;4土特产
+     */
+    private Short belongmodule;
+    
+    /**
+     * 删除状态:0正常;1删除;2用户删除(用户端不显示)
      */
     private Boolean isdelete;
     
@@ -71,139 +77,106 @@ public class ZlOrderDetail implements Serializable {
      */
     private Integer updatedate;
     
+    private static final long serialVersionUID=1L;
     
-    private static final long serialVersionUID = 1L;
-    
-    
-    public Long getOrderdetailid() {
+    public Long getOrderdetailid(){
         return orderdetailid;
     }
     
-    
-    public void setOrderdetailid(Long orderdetailid) {
-        this.orderdetailid = orderdetailid;
+    public void setOrderdetailid(Long orderdetailid){
+        this.orderdetailid=orderdetailid;
     }
     
-    
-    public Long getOrderid() {
+    public Long getOrderid(){
         return orderid;
     }
     
-    
-    public void setOrderid(Long orderid) {
-        this.orderid = orderid;
+    public void setOrderid(Long orderid){
+        this.orderid=orderid;
     }
     
-    
-    public Long getUserid() {
+    public Long getUserid(){
         return userid;
     }
     
-    
-    public void setUserid(Long userid) {
-        this.userid = userid;
+    public void setUserid(Long userid){
+        this.userid=userid;
     }
     
-    
-    public Integer getHotelgoodsid() {
+    public Integer getHotelgoodsid(){
         return hotelgoodsid;
     }
     
-    
-    public void setHotelgoodsid(Integer hotelgoodsid) {
-        this.hotelgoodsid = hotelgoodsid;
+    public void setHotelgoodsid(Integer hotelgoodsid){
+        this.hotelgoodsid=hotelgoodsid;
     }
     
-    
-    public String getGoodsname() {
+    public String getGoodsname(){
         return goodsname;
     }
     
-    
-    public void setGoodsname(String goodsname) {
-        this.goodsname = goodsname == null ? null : goodsname.trim();
+    public void setGoodsname(String goodsname){
+        this.goodsname=goodsname;
     }
     
-    
-    public String getGoodscoverurl() {
+    public String getGoodscoverurl(){
         return goodscoverurl;
     }
     
-    
-    public void setGoodscoverurl(String goodscoverurl) {
-        this.goodscoverurl = goodscoverurl == null ? null : goodscoverurl.trim();
+    public void setGoodscoverurl(String goodscoverurl){
+        this.goodscoverurl=goodscoverurl;
     }
     
-    
-    public BigDecimal getPrice() {
+    public BigDecimal getPrice(){
         return price;
     }
     
-    
-    public void setPrice(BigDecimal price) {
-        this.price = price;
+    public void setPrice(BigDecimal price){
+        this.price=price;
     }
     
-    
-    public Integer getGoodscount() {
+    public Integer getGoodscount(){
         return goodscount;
     }
     
-    
-    public void setGoodscount(Integer goodscount) {
-        this.goodscount = goodscount;
+    public void setGoodscount(Integer goodscount){
+        this.goodscount=goodscount;
     }
     
+    public Short getBelongmodule(){
+        return belongmodule;
+    }
     
-    public Boolean getIsdelete() {
+    public void setBelongmodule(Short belongmodule){
+        this.belongmodule=belongmodule;
+    }
+    
+    public Boolean getIsdelete(){
         return isdelete;
     }
     
-    
-    public void setIsdelete(Boolean isdelete) {
-        this.isdelete = isdelete;
+    public void setIsdelete(Boolean isdelete){
+        this.isdelete=isdelete;
     }
     
-    
-    public Integer getCreatedate() {
+    public Integer getCreatedate(){
         return createdate;
     }
     
-    
-    public void setCreatedate(Integer createdate) {
-        this.createdate = createdate;
+    public void setCreatedate(Integer createdate){
+        this.createdate=createdate;
     }
     
-    
-    public Integer getUpdatedate() {
+    public Integer getUpdatedate(){
         return updatedate;
     }
     
-    
-    public void setUpdatedate(Integer updatedate) {
-        this.updatedate = updatedate;
+    public void setUpdatedate(Integer updatedate){
+        this.updatedate=updatedate;
     }
     
-    
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", orderdetailid=").append(orderdetailid);
-        sb.append(", orderid=").append(orderid);
-        sb.append(", userid=").append(userid);
-        sb.append(", hotelgoodsid=").append(hotelgoodsid);
-        sb.append(", goodsname=").append(goodsname);
-        sb.append(", goodscoverurl=").append(goodscoverurl);
-        sb.append(", price=").append(price);
-        sb.append(", goodscount=").append(goodscount);
-        sb.append(", isdelete=").append(isdelete);
-        sb.append(", createdate=").append(createdate);
-        sb.append(", updatedate=").append(updatedate);
-        sb.append(", serialVersionUID=").append(serialVersionUID);
-        sb.append("]");
-        return sb.toString();
+    public static long getSerialVersionUID(){
+        return serialVersionUID;
     }
+    
 }
