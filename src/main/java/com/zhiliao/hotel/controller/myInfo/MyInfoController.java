b0970f574f74e5191903021e8ee3c20e8b05beb6
LinSir <1689686109@qq.com>
2020-06-03 15:34:50 +0800

    完成“我的”界面信息接口

--- src/main/java/com/zhiliao/hotel/controller/myInfo/MyInfoController.java
+++ src/main/java/com/zhiliao/hotel/controller/myInfo/MyInfoController.java
@@ -12,7 +12,7 @@ import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -50,7 +50,7 @@ public class MyInfoController{
     })
     // @UserLoginToken
     @PassToken
-    @GetMapping("Info")
+    @PostMapping("Info")
     public ReturnString findAllOrder(HttpServletRequest request,Integer hotelID){
         
         try{
